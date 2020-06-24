8e199be8b3300af132a4b9f513ee7279e134e420
Zac Crumpton <z.crumpton@gmail.com>
2020-06-23 19:39:18 -0500

    added routes

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import './App.scss';
 
 import Auth from '../components/pages/Auth/Auth';
@@ -10,10 +17,25 @@ import EditList from '../components/pages/EditList/EditList';
 import Home from '../components/pages/Home/Home';
 import NewList from '../components/pages/NewList/NewList';
 import SingleList from '../components/pages/SingleList/SingleList';
+import MyLists from '../components/pages/MyLists/MyLists';
 
 import fbConnection from '../helpers/data/connection';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to ={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -35,15 +57,30 @@ class App extends React.Component {
   }
 
   render() {
+    const { authed } = this.state;
     return (
       <div className="App">
-        <MyNavbar />
-        <h1>Konichiwa Anime</h1>
-        <Auth />
-        <EditList />
-        <Home />
-        <NewList />
-        <SingleList />
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar />
+            <div className="container">
+              <div className="row">
+              <Switch>
+                <PrivateRoute path='/home' component={Home} authed={authed} />
+                <PrivateRoute path='/list/edit/:listId' component={EditList} authed={authed} />
+                <PrivateRoute path='/list/new' component={NewList} authed={authed} />
+                <PrivateRoute path='/list/:listId' component={SingleList} authed={authed} />
+                <PrivateRoute path='/list' component={MyLists} authed={authed} />
+                {/* <PrivateRoute path='/anime/edit/:animeId' component={EditAnime} authed={authed} />
+                <PrivateRoute path='/anime/new' component={NewAnimeForm} authed={authed} />
+                <PrivateRoute path='/anime/:animeId' component={SingleAnime} authed={authed} /> */}
+                <PublicRoute path='/auth' component={Auth} authed={authed} />
+                <Redirect from="*" to="/home"/>
+            </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/pages/MyLists/MyLists.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './MyLists.scss';
+
+class MyLists extends React.Component {
+  render() {
+    return (
+      <div className="MyLists">
+        <h1>My Lists</h1>
+      </div>
+    );
+  }
+}
+
+export default MyLists;
