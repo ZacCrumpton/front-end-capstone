f072a8c6e2ef2e5cc6dded67f9ddc5a06b96ca8e
Zac Crumpton <z.crumpton@gmail.com>
2020-06-23 19:20:46 -0500

    added initial components

---
+++ src/components/pages/NewList/NewList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './NewList.scss';
+
+class NewList extends React.Component {
+  render() {
+    return (
+      <div className="NewList">
+        <h1>New List</h1>
+      </div>
+    );
+  }
+}
+
+export default NewList;
---
+++ src/components/pages/SingleList/SingleList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './SingleList.scss';
+
+class SingleList extends React.Component {
+  render() {
+    return (
+      <div className="SingleList">
+        <h1>Single List</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleList;
