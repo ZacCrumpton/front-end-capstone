4dcbff90bcf99b5351af135a8922266a9f16517e
Zac Crumpton <z.crumpton@gmail.com>
2020-06-23 19:44:24 -0500

    edit list route works

--- src/components/pages/EditList/EditList.js
+++ src/components/pages/EditList/EditList.js
@@ -4,9 +4,11 @@ import './EditList.scss';
 
 class EditList extends React.Component {
   render() {
+    const editId = this.props.match.params.listId;
     return (
       <div className="EditList">
         <h1>Edit List</h1>
+    <h2>The List Id is {editId}</h2>
       </div>
     );
   }
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,10 +3,17 @@ import React from 'react';
 import './Home.scss';
 
 class Home extends React.Component {
+  editEvent = (e) => {
+    e.preventDefault();
+    const listId = 'list45665436';
+    this.props.history.push(`/list/edit/${listId}`);
+  }
+
   render() {
     return (
       <div className="Home">
         <h1>Home</h1>
+        <button className="btn btn-dark" onClick={this.editEvent}>Edit a List</button>
       </div>
     );
   }
