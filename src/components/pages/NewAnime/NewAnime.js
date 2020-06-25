d33d47fcedbbf54210d90dcc58e5371b5509e0de
Zac Crumpton <z.crumpton@gmail.com>
2020-06-24 23:51:59 -0500

    added components for anime

---
+++ src/components/pages/NewAnime/NewAnime.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './NewAnime.scss';
+
+class NewAnime extends React.Component {
+  render() {
+    return (
+      <div className="NewAnime">
+        <h1>NewAnime</h1>
+      </div>
+    );
+  }
+}
+
+export default NewAnime;
