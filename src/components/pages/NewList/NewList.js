26fa49c4916e2b01b2ba2b353cb3b25f54e8d33c
Zac Crumpton <z.crumpton@gmail.com>
2020-06-24 21:38:27 -0500

    added form and input fields for new list

--- src/components/pages/NewList/NewList.js
+++ src/components/pages/NewList/NewList.js
@@ -3,10 +3,36 @@ import React from 'react';
 import './NewList.scss';
 
 class NewList extends React.Component {
+  state = {
+    listTitle: '',
+  }
+
+  titleChange = (e) => {
+    e.preventDefault();
+    this.setState({ listTitle: e.target.value });
+  }
+
   render() {
+    const {
+      listTitle,
+    } = this.state;
+
     return (
       <div className="NewList">
         <h1>New List</h1>
+        <form className="col-12 offset-3 text-left">
+          <div className="form-group">
+            <label htmlFor="list-title">Title</label>
+            <input
+            type="text"
+            className="form-control"
+            id="list-title"
+            value={listTitle}
+            onChange={this.titleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-dark">submit</button>
+        </form>
       </div>
     );
   }
