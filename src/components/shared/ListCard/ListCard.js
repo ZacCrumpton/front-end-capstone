86af286134248011c6bbba16a0620ffb73a19e2e
Zac Crumpton <z.crumpton@gmail.com>
2020-06-24 21:16:23 -0500

    added edit button to list card

--- src/components/shared/ListCard/ListCard.js
+++ src/components/shared/ListCard/ListCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import listShape from '../../../helpers/propz/listShape';
 
@@ -11,11 +12,13 @@ class ListCard extends React.Component {
 
   render() {
     const { list } = this.props;
+    const editLink = `/list/edit/${list.id}`;
     return (
       <div className="ListCard col-4">
         <div className="card">
           <div className="card-body">
-    <h6 className="card-title">{list.title}</h6>
+            <h6 className="card-title">{list.title}</h6>
+            <Link className="btn btn-dark" to={editLink}>Edit</Link>
           </div>
         </div>
       </div>
